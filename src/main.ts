fbc8f3092377ed597140352cb233caedca82eb95
sihun <thrchtlgns@naver.com>
2023-03-27 21:01:11 +0900

    feat install morgan

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'))
   await app.listen(3000);
 }
 bootstrap();
