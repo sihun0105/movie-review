f50e089e052d06777b1ec88f985ec70dea4aee52
sihun <thrchtlgns@naver.com>
2023-04-02 15:46:02 +0900

    feat 스웨거 추가

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('MOVIE REVIEW API')
+    .setDescription('MOVIE REVIEW API 정리 문서')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.use(morgan('dev'))
   await app.listen(3000);
 }
