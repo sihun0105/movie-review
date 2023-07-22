46e33f5831849c73adb00c1a409f1a1c4517c67f
sihun <thrchtlgns@naver.com>
2023-07-22 15:13:16 +0900

    feat 영화목록 return 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,28 +1,37 @@
-import { CACHE_MANAGER, Controller, Get, Inject, Query, UseGuards } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  Controller,
+  Get,
+  Inject,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-import { Cache } from 'cache-manager'
+import { Cache } from 'cache-manager';
 import { MovieService } from './movie/movie.service';
 import { Cron } from '@nestjs/schedule';
+import { JwtAuthGuard } from './auth/JwtAuthGuard';
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private readonly movieService : MovieService,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache
-    ) {}
+    private readonly movieService: MovieService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
   @Get('/')
+  @UseGuards(JwtAuthGuard)
   getMoviedataSelf() {
-    const nowdate = new Date();
-    let formattedDate = nowdate.getFullYear().toString() + (nowdate.getMonth() + 1).toString().padStart(2, '0') + (nowdate.getDate() - 1).toString().padStart(2, '0');
-    console.log(formattedDate)
-    return this.movieService.fetchMovies(formattedDate);
+    return this.movieService.getMovies();
   }
 
   @Cron('0 0 * * *')
   getMoviedata() {
     console.log('Executing task once a day');
     const nowdate = new Date();
-    let formattedDate = nowdate.getFullYear().toString() + (nowdate.getMonth() + 1).toString().padStart(2, '0') + (nowdate.getDate() - 1).toString(). padStart(2, '0');
+    let formattedDate =
+      nowdate.getFullYear().toString() +
+      (nowdate.getMonth() + 1).toString().padStart(2, '0') +
+      (nowdate.getDate() - 1).toString().padStart(2, '0');
     this.movieService.fetchMovies(formattedDate);
   }
 }
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -9,28 +9,32 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class MovieService {
   private readonly apiKey = process.env.MOVIE_SECRET;
-  private readonly baseUrl = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json';
+  private readonly baseUrl =
+    'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json';
 
   constructor(
     private httpService: HttpService,
     @InjectRepository(Movie) private movieRepository: Repository<Movie>,
   ) {}
-
+  async getMovies() {
+    const movieList = await this.movieRepository.find({});
+    console.log(movieList);
+    return movieList;
+  }
   async fetchMovies(date: string): Promise<void> {
     const url = `${this.baseUrl}?key=${this.apiKey}&targetDt=${date}`;
     const response: AxiosResponse = await this.httpService.get(url).toPromise();
-    
+
     const movieList = response.data.boxOfficeResult.dailyBoxOfficeList;
 
-    await this.movieRepository.delete({})
+    await this.movieRepository.delete({});
     for (const movieData of movieList) {
       const movie = new Movie();
       //const checkMovie = await this.movieRepository.findOne({where:{movieCd:movieData.movieCd}})
-        movie.movieCd = movieData.movieCd;
-        movie.title = movieData.movieNm;
-        movie.audience = movieData.audiAcc;
-      await this.movieRepository.save(movie)
+      movie.movieCd = movieData.movieCd;
+      movie.title = movieData.movieNm;
+      movie.audience = movieData.audiAcc;
+      await this.movieRepository.save(movie);
     }
   }
 }
-
