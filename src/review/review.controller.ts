c27f077e3e64ae0e88144f9d382188c59b547f22
KIMSIHUN <tlgns14@nate.com>
2023-11-03 15:45:34 +0900

    ♻️ typeorm -> prisma 마이그레이션

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body, Delete } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -4,10 +4,11 @@ import { ReviewController } from './review.controller';
 import { Movie } from 'src/entities/movie.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from 'src/entities/commnet.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Movie, Comment])],
   controllers: [ReviewController],
-  providers: [ReviewService],
+  providers: [ReviewService, PrismaService],
 })
 export class ReviewModule {}
