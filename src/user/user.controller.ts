5dd0c8ec73b28ab2c46f2a054f95efe24e672bfb
KIMSIHUN <tlgns14@nate.com>
2024-01-07 17:09:21 +0900

    🎨닉네임 중복검사 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,4 +4,12 @@ import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  async verifyNickname(nickname: string) {
+    const user = await this.userService.verifyNickname(nickname);
+    if (!user) {
+      return true;
+    }
+    return false;
+  }
 }
