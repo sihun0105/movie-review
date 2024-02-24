a244e534996a1e518dd41c6008d7d3454b097954
KIMSIHUN <tlgns14@nate.com>
2024-02-24 19:24:30 +0900

    ♻️ 리뷰 컨트롤러에 인증 가드 추가 및 사용자 관련 코드 정리

--- src/domain/review/review.controller.ts
+++ src/domain/review/review.controller.ts
@@ -15,10 +15,10 @@ import { UpdateReviewDto } from './dto/update-review.dto';
 import { JwtAuthGuard } from 'src/guard/jwt-auth.guard';
 
 @Controller('review')
+@UseGuards(JwtAuthGuard)
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
   @Get('')
-  @UseGuards(JwtAuthGuard)
   getReviewByMovieId(@Query('movieId') movieId: number) {
     const reviewData = this.reviewService.getReviewByMovieId(movieId);
     if (!reviewData) {
@@ -28,21 +28,18 @@ export class ReviewController {
   }
 
   @Post('')
-  @UseGuards(JwtAuthGuard)
   create(@Request() req, @Body() createReviewDto: CreateReviewDto) {
     const userId = req.user.userid;
     return this.reviewService.create({ data: createReviewDto, userId: userId });
   }
 
   @Post('/update')
-  @UseGuards(JwtAuthGuard)
   update(@Request() req, @Body() updateReviewDto: UpdateReviewDto) {
     const userId = req.user.userid;
     return this.reviewService.update({ data: updateReviewDto, userId: userId });
   }
 
   @Delete(':commentId')
-  @UseGuards(JwtAuthGuard)
   remove(@Request() req, @Param() { commentId }: { commentId: number }) {
     const userId = req.user.userid;
     return this.reviewService.remove({ commentId, userId });
